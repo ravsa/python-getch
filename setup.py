0dabc5c8e8534fe6efdab593b2eda48ecae41283
ravsa <rvsbana@gmail.com>
2015-12-21 22:05:14 +0530

    distutils setup script for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext = Extension('getch',
+                sources=['getch.pyx'],
+                )
+
+setup(ext_modules=[ext],
+      cmdclass={'build_ext': build_ext},
+      )
